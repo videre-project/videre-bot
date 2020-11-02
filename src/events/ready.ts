a7c132aabf479632a01df8fd311d78a2c0cff333
Cory Bennett <32466081+Qonfused@users.noreply.github.com>
2020-11-02 12:16:43 -0600

    Fix clipping

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,6 +1,6 @@
 import { Bot } from '../bot'
 
 export async function run(client: Bot) {
-  client.user.setActivity('VidereProject.com | !help')
+  client.user.setActivity('VidereProject.com')
   console.log(`Connected as ${client.user.tag}`)
 }
