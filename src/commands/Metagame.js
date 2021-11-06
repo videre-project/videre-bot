0d9001b50e8ef9e3058e3933487c9406cfdb6bac
Cory Bennett <32466081+Qonfused@users.noreply.github.com>
2021-11-05 23:34:53 -0500

    Add date_range display to /metagame output

--- src/commands/Metagame.js
+++ src/commands/Metagame.js
@@ -52,7 +52,15 @@ const Metagame = {
                             : ''
                 };
             }
-            const { format, time_interval } = response.parameters;
+            const { format, time_interval, offset } = response.parameters;
+            const date_range = response
+                .data[format.toLowerCase()]
+                .events.data.map(obj => obj.date)
+                .sort((a, b) => {
+                    var aa = a.split('/').reverse().join(),
+                        bb = b.split('/').reverse().join();
+                    return aa < bb ? -1 : (aa > bb ? 1 : 0);
+                });
 
             const data = response
                 .data[format.toLowerCase()]
@@ -67,8 +75,13 @@ const Metagame = {
                     .map(page => ({
                         name: [
                             'Top archetypes in', format.charAt(0).toUpperCase() + format.slice(1),
-                            'in the last', time_interval, time_interval == 1 ? 'day' : 'days'
-                        ].join(' ') + ':',
+                            'in the last', time_interval, (time_interval == 1 ? 'day' : 'days')
+                        ].join(' ') + ':\n'
+                        + (date_range?.length
+                            ? '\`(' + date_range[0] + ' to ' + date_range.slice(-1)[0]
+                                + (offset ? `; offset by ${ offset } ${ offset == 1 ? 'day' : 'days' }` : '')
+                                + ')\`'
+                            : ''),
                         value: `\`\`\`diff\n${ page.join('\n') }\n\`\`\``,
                     })),
                 { title: 'Metagame' }, 0, 1, 'fields',
