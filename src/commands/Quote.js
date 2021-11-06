271bb0e550f999e2cae96001659c01e58b9b4a99
Cory Bennett <32466081+Qonfused@users.noreply.github.com>
2021-11-06 01:25:24 -0500

    Add 'hide_original' option to /quote

--- src/commands/Quote.js
+++ src/commands/Quote.js
@@ -9,22 +9,28 @@ const Quote = {
             type: 'string',
             required: true,
         },
+        {
+            name: 'hide_original',
+            description: 'Hide original message (Shows only \'jump to link\' hyperlink).',
+            type: 'boolean',
+        },
     ],
     async execute({ client, args }) {
         const message_link = args?.message_link;
-        try {
+        const hide_original = args?.hide_original;
+        // try {
             const [guildID, channelID, messageID] = message_link.split('discord.com/channels/')[1].split('/');
 
             const channel = await client.channels.fetch(channelID);
             const message = await channel.messages.fetch(messageID);
 
-            const avatarURL = `https://cdn.discordapp.com/avatars/${message.author.id}/${message.author.avatar}.webp?size=128`;
-            const channelIcon = `https://cdn.discordapp.com/icons/${guildID}/${message.guild.icon}.webp?size=128`;
+            const avatarURL = `https://cdn.discordapp.com/avatars/${ message.author.id }/${ message.author.avatar }.webp?size=128`;
+            const channelIcon = `https://cdn.discordapp.com/icons/${ guildID }/${ message.guild.icon }.webp?size=128`;
 
             let embed = {
                 author: {
-                    name: `${message.author.username}#${message.author.discriminator} (Jump to message)`,
-                    url: `https://discord.com/channels/${[guildID, channelID, messageID].join('/')}`,
+                    name: `${message.author.username}#${ message.author.discriminator } (Jump to message)`,
+                    url: `https://discord.com/channels/${ [guildID, channelID, messageID].join('/') }`,
                     icon_url: avatarURL,
                 },
                 footer: {
@@ -34,46 +40,58 @@ const Quote = {
                 timestamp: new Date(message.createdTimestamp).toISOString()
             };
 
-            if (message?.embeds.length > 0 && message?.embeds[0]?.type == 'rich') {
-                if (message.embeds.length == 1) {
-                    if (message.embeds[0]?.title) embed.title = message.embeds[0].title;
-                    if (message.embeds[0]?.url) embed.url = message.embeds[0].url;
-                    if (message.embeds[0]?.description) embed.description = message.embeds[0].description;
-                    if (message.embeds[0]?.fields) embed.fields = message.embeds[0].fields;
-                    if (message.embeds[0]?.image) embed.image = message.embeds[0].image;
-                    if (message.embeds[0]?.thumbnail) embed.thumbnail = message.embeds[0].thumbnail;
-                } else {
-                    embed.description = `\`[ Embeds: ${message.embeds.length} ]\``;
+            if (!(hide_original)) {
+                if (message?.embeds.length > 0 && message?.embeds[0]?.type == 'rich') {
+                    if (message.embeds.length == 1) {
+                        if (message.embeds[0]?.title) embed.title = message.embeds[0].title;
+                        if (message.embeds[0]?.url) embed.url = message.embeds[0].url;
+                        if (message.embeds[0]?.description) embed.description = message.embeds[0].description;
+                        if (message.embeds[0]?.fields) embed.fields = message.embeds[0].fields;
+                        if (message.embeds[0]?.image) embed.image = message.embeds[0].image;
+                        if (message.embeds[0]?.thumbnail) embed.thumbnail = message.embeds[0].thumbnail;
+                    } else {
+                        embed.description = `\`[ Embeds: ${ message.embeds.length } ]\``;
+                    }
+                    if (!message?.content) return embed;
                 }
-                if (!message?.content) return embed;
-            }
 
-            if (message?.content) {
-                if (message.content.match(/.(jpg|jpeg|png|gif)$/i) && (message.content.match(/ /g) || []).length == 0) {
-                    embed.image = { 'url': message.content };
-                } else {
-                    embed.description = message.content;
+                if (message?.content) {
+                    if (message.content.match(/.(jpg|jpeg|png|gif)$/i) && (message.content.match(/ /g) || []).length == 0) {
+                        embed.image = { 'url': message.content };
+                    } else {
+                        embed.description = message.content;
+                    }
                 }
-            }
 
-            if (message?.attachments.array().length > 0) {
-                if (message.attachments.array()[0]?.height > 0) {
-                    embed.image = { 'url': message.attachments.array()[0].url };
-                } else {
-                    embed.description = `\`[ Attachment: ${message.attachments.array()[0].name} ]\``;
+                if (message?.attachments.array().length > 0) {
+                    if (message.attachments.array()[0]?.height > 0) {
+                        embed.image = { 'url': message.attachments.array()[0].url };
+                    } else {
+                        embed.description = `\`[ Attachment: ${ message.attachments.array()[0].name } ]\``;
+                    }
                 }
+            } else {
+                const content_char_length = message?.content ? message.content.length : '';
+                const embed_char_length = message?.embeds?.[0] ? message.embeds[0].length : '';
+                const char_count = content_char_length + embed_char_length;
+                embed.description = `\`[${[
+                        message?.embeds?.length
+                            ? `${ message.embeds.length } ${ message.embeds.length == 1 ? 'embed' : 'embeds' };`
+                            : undefined,
+                        `${ char_count } ${ char_count == 1 ? 'character' : 'characters' }`
+                    ].join(' ')}]\``;
             }
 
             return embed;
 
-        } catch (error) {
-            return {
-                title: 'Error',
-                description: `An error occured while quoting a message.`,
-                color: 0xe74c3c,
-                ephemeral: true,
-            };
-        }
+        // } catch (error) {
+        //     return {
+        //         title: 'Error',
+        //         description: `An error occured while quoting a message.`,
+        //         color: 0xe74c3c,
+        //         ephemeral: true,
+        //     };
+        // }
     },
 };
 
